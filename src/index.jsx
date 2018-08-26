9533e0d0232745d4015067e3bce20aef5895ebbd
johadi <jimoh.hadi@gmail.com>
2018-08-26 11:06:13 +0100

    integrate site boilerplate

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import '../public/assets/styles/style.scss';
+// import '../public/assets/styles/style.scss';
 import App from './components/App';
 
 ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.querySelector('#app'));
