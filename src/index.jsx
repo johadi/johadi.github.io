edb7f2962f22a9d4968b2846d54a628f7dbbf441
johadi <jim.hadi10@gmail.com>
2019-09-28 16:01:27 +0100

    Remove space

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 // import '../public/assets/styles/style.scss';
-
 import App from './components/App';
 
 ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.querySelector('#app'));
