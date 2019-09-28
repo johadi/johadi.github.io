2851adca739455ae8be172811ff7e97f10f4d660
johadi <jim.hadi10@gmail.com>
2019-09-28 15:58:59 +0100

    Update ndex file

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 // import '../public/assets/styles/style.scss';
+
 import App from './components/App';
 
 ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.querySelector('#app'));
